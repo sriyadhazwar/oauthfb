c09690fa317932fd321ffc7c939ebc6f04153680
sriyadhazwar <96353147+sriyadhazwar@users.noreply.github.com>
2022-03-25 18:31:44 +0700

    Add files via upload

---
+++ configuration/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+ 
+
+  facebook_api_key: "1032149550715546",
+  facebook_api_secret: "006ba2b85b68fc290aa06f33525713b8",
+  callback_url: "http://localhost:3000/auth/facebook/callback",
+  use_database: false,
+  host: "localhost",
+  username: "root",
+  password: "",
+  database: ""
+};
